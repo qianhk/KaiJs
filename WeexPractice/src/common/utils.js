71fc734baf69db054789f41f6abf6ff564039f46
feiatc <qhk83@qq.com>
2016-10-11 21:02:45 +0800

    test user common js

---
+++ WeexPractice/src/common/utils.js
@@ -0,0 +1,13 @@
+
+function add(arg1, arg2) {
+    return arg1 + arg2;
+}
+
+function minus(arg1, arg2) {
+    return arg1 - arg2;
+}
+
+module.exports = {
+        add: add,
+        minus: minus
+};
