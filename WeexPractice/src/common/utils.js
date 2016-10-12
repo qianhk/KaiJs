4f432567d5471b66ab0b782dc2cfa692b9aa43bc
feiatc <qhk83@qq.com>
2016-10-12 11:27:53 +0800

    test use weex modal from js file

--- WeexPractice/src/common/utils.js
+++ WeexPractice/src/common/utils.js
@@ -1,4 +1,9 @@
 
+var modal;
+__weex_define__('@weex-temp/xyz', function (__weex_require__) {
+  modal = __weex_require__('@weex-module/modal');
+});
+
 function add(arg1, arg2) {
     return arg1 + arg2;
 }
@@ -7,7 +12,14 @@ function minus(arg1, arg2) {
     return arg1 - arg2;
 }
 
+function toast(message) {
+    // let modal = require('@weex-module/modal');
+    modal.toast({message:message, length:1});
+}
+
+
 module.exports = {
         add: add,
-        minus: minus
+        minus: minus,
+        toast: toast,
 };
