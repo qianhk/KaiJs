aa301c39a89dda0a544575df01a04c57149b9b59
feiatc <qhk83@qq.com>
2016-12-20 20:13:35 +0800

    test node express make api

--- NodeExpress/routes/index.js
+++ NodeExpress/routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'ExpressTitle'});
+});
+
+router.get('/listUsers', function (req, res) {
+    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function (err, data) {
+        console.log(data);
+        res.end(data);
+    });
 });
 
 module.exports = router;
--- NodeExpress/routes/users.js
+++ NodeExpress/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  let dataPath = path.join(__dirname, '../bin/data/');
+  console.log(dataPath);
+  fs.readFile(dataPath + "users.json", 'utf8', function (err, data) {
+    // console.log(data);
+    res.end(data);
+    // res.send('respond with a resource: ' + data + " dataEnd.");
+  });
 });
 
 module.exports = router;
