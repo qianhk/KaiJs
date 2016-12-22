24e701f8b53e2ce8b73b65534158c4de393c43d0
qianhk <qhk83@qq.com>
2016-12-22 21:59:06 +0800

    test post method for express

--- NodeExpress/routes/index.js
+++ NodeExpress/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/test', function (req, res) {
+    res.end('you use post method');
+})
+
 module.exports = router;
