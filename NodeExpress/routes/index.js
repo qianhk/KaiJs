49f18f8047c035c0fc10ea4c6363930254bd65ee
feiatc <qhk83@qq.com>
2016-12-23 10:34:14 +0800

    test express rest api

--- NodeExpress/routes/index.js
+++ NodeExpress/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -7,14 +8,33 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/listUsers', function (req, res) {
-    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function (err, data) {
-        console.log(data);
+    let userJsonFilePath = __dirname + "/../bin/data/" + "users.json";
+    console.log('look path: ' + userJsonFilePath);
+    fs.readFile(userJsonFilePath, 'utf8', function (err, data) {
+        // console.log(data);
         res.end(data);
     });
 });
 
+router.get('/test/:name', function (req, res) {
+    res.end('print: ' + req.params.name);
+});
+
+router.get('/test', function (req, res) {
+    res.end('print: ' + 'get no other');
+});
+
 router.post('/test', function (req, res) {
-    res.end('you use post method');
-})
+    var name = req.body.name;
+    res.json({message: 'Hello ' + name});
+});
+
+router.put('/test', function (req, res) {
+    res.end('print: ' + 'put no other');
+});
+
+router.delete('/test', function (req, res) {
+    res.end('print: ' + 'delete no other');
+});
 
 module.exports = router;
