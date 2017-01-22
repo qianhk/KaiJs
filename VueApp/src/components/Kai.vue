b20803a9b163d5c9e1ba99e75c34a594d3089b47
feiatc <qhk83@qq.com>
2017-01-22 16:38:12 +0800

    test transition effect again

--- VueApp/src/components/Kai.vue
+++ VueApp/src/components/Kai.vue
@@ -4,6 +4,9 @@
     <transition name="fade">
       <p v-if="show">hello</p>
     </transition>
+    <transition name="slide-fade">
+      <p v-if="show">hello2</p>
+    </transition>
   </div>
 </template>
 
@@ -31,9 +34,22 @@
 
 <style>
   .fade-enter-active, .fade-leave-active {
-    transition: opacity 0.5s
+    transition: opacity 1.5s
   }
   .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
     opacity: 0
   }
+
+  .slide-fade-enter-active {
+    transition: all .3s ease;
+  }
+  .slide-fade-leave-active {
+    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+  }
+  .slide-fade-enter, .slide-fade-leave-to
+    /* .slide-fade-leave-active for <2.1.8 */ {
+    transform: translateX(10px);
+    opacity: 0;
+  }
+
 </style>
