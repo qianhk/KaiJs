447146f82841260cfc142660da4d7da6ca7efb0a
qianhk <qhk83@qq.com>
2017-07-01 15:30:20 +0800

    test vue

---
+++ WeexPractice/src/stateview.vue
@@ -0,0 +1,42 @@
+<template>
+    <div style="background-color: transparent">
+        <div style="flex-direction: row; justify-content: space-between;">
+            <text style="background-color: palegoldenrod; color: dodgerblue; height: 50; text-align: center;" @click="onChangeState('LOADING')">LOADING</text>
+            <text style="background-color: palegoldenrod; color: dodgerblue; height: 50; justify-content: center;" @click="onChangeState('NO_DATA')">NO_DATA</text>
+            <text style="background-color: palegoldenrod; color: dodgerblue; height: 50; align-items: center" @click="onChangeState('FAILED')">FAILED</text>
+            <text style="background-color: palegoldenrod; color: dodgerblue; height: 50;" @click="onChangeState('SUCCESS')">SUCCESS</text>
+        </div>
+        <pw-state-view :state="state" empty-text="测试空文字" @retry="onRetry" style="flex: 1;">
+
+            <div style="align-items: center; justify-content: center; flex: 1; width: 750; height: 200;">
+                <text>success view</text>
+            </div>
+        </pw-state-view>
+    </div>
+</template>
+
+<script>
+    const toast = weex.requireModule('pw-toast')
+
+    module.exports = {
+        methods: {
+            onRetry: function () {
+                toast.toast('onRetry normal')
+            },
+            onChangeState: function (state) {
+                this.state = state;
+            }
+        },
+        created: function () {
+            console.log('created config: ' + JSON.stringify(this.$getConfig()));
+        },
+        data: function () {
+            return {
+                state: 'LOADING',
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
