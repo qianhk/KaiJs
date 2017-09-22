0c4b4268641850b156b4c55074a246527e2c419b
feiatc <qhk83@qq.com>
2017-09-22 15:45:11 +0800

    test lyric

---
+++ WeexPractice/src/lyric.vue
@@ -0,0 +1,22 @@
+<template>
+    <div style="background-color: lightgreen;">
+
+        <text style="text-align: center;">header</text>
+
+        <lyric style="flex:1; background-color: dodgerblue" file-path="/sdcard/qhk.trc"></lyric>
+
+        <text style="text-align: center;">footer</text>
+
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data: {},
+        methods: {}
+    }
+</script>
+
+<style>
+
+</style>
