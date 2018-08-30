9b618eaf05334b073ba8e90f079170f85604a1b4
feiatc <qhk83@qq.com>
2018-08-30 14:07:12 +0800

    test new js regular expression

--- JsPractice/test-regular.js
+++ JsPractice/test-regular.js
@@ -32,3 +32,23 @@ console.log(re.lastIndex);
 //   // expected output: "Found foo. Next starts at 19."
 // }
 
+// ECMAScript正则表达式6个最新特性
+// https://blog.fundebug.com/2018/08/30/ecmascript-regular-expression-new-features/?hmsr=toutiao.io&utm_medium=toutiao.io&utm_source=toutiao.io
+
+const pattern1 = /(\d{4})-(\d{2})-(\d{2})/u;
+const result1 = pattern1.exec('2017-01-25');
+console.log(result1[0]); // 打印"2017-01-25"
+console.log(result1[1]); // 打印"2017"
+console.log(result1[2]); // 打印"01"
+console.log(result1[3]); // 打印"25"
+
+
+const pattern2 = /(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/u;
+const result2 = pattern2.exec('2017-01-25');
+console.log(result2.groups.year); // 打印"2017"
+console.log(result2.groups.month); // 打印"01"
+console.log(result2.groups.day); // 打印"25"
+
+for (let value of result1) {  // weex的sdk js环境不支持这个写法
+  console.log(`result1_value=${value}`);
+}
